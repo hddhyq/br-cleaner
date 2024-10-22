881a557d3b5ead0119aeeca379e896c958fb3f79
Brokenbones dd <brokenbonesdd@BrokenbonesdeMacBook-Pro.local>
2024-10-22 11:41:24 +0800

    chore: release v0.0.1

--- README.md
+++ README.md
@@ -1,37 +1,28 @@
-# pkg-placeholder
+# br-cleaner
 
-[![npm version][npm-version-src]][npm-version-href]
-[![npm downloads][npm-downloads-src]][npm-downloads-href]
-[![bundle][bundle-src]][bundle-href]
-[![JSDocs][jsdocs-src]][jsdocs-href]
-[![License][license-src]][license-href]
+git branch cleaner
 
-_description_
+## Installation
 
-> **Note**:
-> Replace `pkg-placeholder`, `_description_` and `antfu` globally to use this template.
+```bash
+npm install -g br-cleaner
+```
 
-## Sponsors
+## Usage
 
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
+```bash
+br-cleaner
+```
 
-## License
+## features
+### select remote branches when have remote branches
 
-[MIT](./LICENSE) License © 2024-PRESENT [Anthony Fu](https://github.com/antfu)
+### filter branches
+- filter branches by name
 
-<!-- Badges -->
+### choose branches
+- choose branches to delete
 
-[npm-version-src]: https://img.shields.io/npm/v/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669
-[npm-version-href]: https://npmjs.com/package/pkg-placeholder
-[npm-downloads-src]: https://img.shields.io/npm/dm/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669
-[npm-downloads-href]: https://npmjs.com/package/pkg-placeholder
-[bundle-src]: https://img.shields.io/bundlephobia/minzip/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669&label=minzip
-[bundle-href]: https://bundlephobia.com/result?p=pkg-placeholder
-[license-src]: https://img.shields.io/github/license/antfu/pkg-placeholder.svg?style=flat&colorA=080f12&colorB=1fa669
-[license-href]: https://github.com/antfu/pkg-placeholder/blob/main/LICENSE
-[jsdocs-src]: https://img.shields.io/badge/jsdocs-reference-080f12?style=flat&colorA=080f12&colorB=1fa669
-[jsdocs-href]: https://www.jsdocs.io/package/pkg-placeholder
+### delete branches
+- delete local branches
+- delete remote branches
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,123 @@
-export const one = 1
-export const two = 2
+import inquirer from 'inquirer'
+import simpleGit from 'simple-git'
+import type { SimpleGit } from 'simple-git'
+
+const git: SimpleGit = simpleGit()
+
+interface Branch {
+  name: string
+  value: string
+  remote: boolean
+}
+
+async function getRemoteNames(): Promise<string[]> {
+  const remotes = await git.getRemotes(true)
+  return remotes.map(remote => remote.name)
+}
+
+async function getBranches(remoteName: string): Promise<Branch[]> {
+  const branches = await git.branch(['-a'])
+  const localBranches = branches.all.filter(branch => !branch.includes('remotes/'))
+  const remoteBranches = branches.all.filter(branch => branch.includes(`remotes/${remoteName}/`))
+
+  const uniqueBranches = localBranches.map((branch) => {
+    const remoteBranch = remoteBranches.find(rb => rb.endsWith(branch))
+    return {
+      name: branch,
+      value: branch,
+      remote: !!remoteBranch,
+    }
+  })
+
+  const onlyRemoteBranches = remoteBranches.filter(rb =>
+    !localBranches.some(lb => rb.endsWith(lb)),
+  ).map(branch => ({
+    name: branch,
+    value: branch,
+    remote: true,
+  }))
+
+  return [...uniqueBranches, ...onlyRemoteBranches]
+}
+
+async function filterBranches(branches: Branch[]): Promise<Branch[]> {
+  const { keyword } = await inquirer.prompt<{ keyword: string }>([{
+    type: 'input',
+    name: 'keyword',
+    message: 'Enter keyword to filter branches:',
+  }])
+
+  return branches.filter(branch => branch.name.includes(keyword))
+}
+
+async function selectBranches(branches: Branch[]): Promise<string[]> {
+  const { selectedBranches } = await inquirer.prompt<{ selectedBranches: string[] }>([{
+    type: 'checkbox',
+    name: 'selectedBranches',
+    message: 'Select branches to delete:',
+    choices: branches,
+  }])
+
+  return selectedBranches
+}
+
+async function deleteBranch(branch: string, remoteName: string): Promise<void> {
+  try {
+    if (branch.startsWith(`remotes/${remoteName}/`)) {
+      const remoteBranch = branch.replace(`remotes/${remoteName}/`, '')
+      await git.push([remoteName, '--delete', remoteBranch])
+      // eslint-disable-next-line no-console
+      console.log(`Deleted remote branch: ${branch}`)
+    }
+    else {
+      await git.branch(['-d', branch])
+      // eslint-disable-next-line no-console
+      console.log(`Deleted local branch: ${branch}`)
+
+      // Also delete the corresponding remote branch if it exists
+      const remoteBranch = `remotes/${remoteName}/${branch}`
+      const allBranches = await getBranches(remoteName)
+      if (allBranches.some(branch => branch.name.includes(remoteBranch))) {
+        await git.push([remoteName, '--delete', branch])
+        // eslint-disable-next-line no-console
+        console.log(`Deleted corresponding remote branch: ${remoteBranch}`)
+      }
+    }
+  }
+  catch (error: any) {
+    console.error(`Failed to delete branch: ${branch}`, error.message)
+  }
+}
+
+async function deleteBranches(branches: string[], remoteName: string): Promise<void> {
+  for (const branch of branches) {
+    await deleteBranch(branch, remoteName)
+  }
+}
+
+async function main(): Promise<void> {
+  try {
+    const remoteNames = await getRemoteNames()
+    let remoteName = 'origin'
+
+    if (remoteNames.length > 1) {
+      const { selectedRemote } = await inquirer.prompt<{ selectedRemote: string }>([{
+        type: 'list',
+        name: 'selectedRemote',
+        message: 'Select the remote to use:',
+        choices: remoteNames,
+      }])
+      remoteName = selectedRemote
+    }
+
+    const allBranches = await getBranches(remoteName)
+    const filteredBranches = await filterBranches(allBranches)
+    const branchesToDelete = await selectBranches(filteredBranches)
+    await deleteBranches(branchesToDelete, remoteName)
+  }
+  catch (error: any) {
+    console.error('Error:', error.message)
+  }
+}
+
+main()
